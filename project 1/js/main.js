95fe10e690f489f3fc1398e08b91f2e211843afe
Sabbir Ahamed <73008358+sabbir72@users.noreply.github.com>
2022-01-15 19:45:16 +0600

    Add files via upload

---
+++ project 1/js/main.js
@@ -0,0 +1,31 @@
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const year = document.querySelector('#year');
+const btn= document.querySelector('.btn');
+const bookList = document.querySelector('#book-List');
+
+btn.addEventListener('click', function(e) {
+    e.preventDefault();
+
+
+    if (title.value==''&& author.value==''&& year.value=='') {
+        console.log(" Input Value please");
+    }else{
+        const newRow = document.createElement('tr');
+
+        const newTitle = document.createElement('th');
+        newTitle.innerHTML= title.value;
+        newRow.appendChild(newTitle);
+
+
+        const newAuthor = document.createElement('th');
+        newAuthor.innerHTML= author.value;
+        newRow.appendChild(newAuthor);
+
+        const newYear = document.createElement('th');
+        newYear.innerHTML= year.value;
+        newRow.appendChild(newYear);
+
+        bookList.appendChild(newRow);
+    }
+});
